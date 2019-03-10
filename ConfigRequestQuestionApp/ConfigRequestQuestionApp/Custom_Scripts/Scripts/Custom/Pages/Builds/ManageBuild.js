c8fe3273a0b8bd54ef164cb74400f15020a8aaff
dsherley9 <34556150+dsherley9@users.noreply.github.com>
2019-03-10 18:05:16 -0500

    Tons of Stuff
    Converting JQuery Selectors to JQuery Objects
    
    More Question Form Updates
    In-progress - Question Options
    Will load available children from children of selected node.

--- ConfigRequestQuestionApp/ConfigRequestQuestionApp/Custom_Scripts/Scripts/Custom/Pages/Builds/ManageBuild.js
+++ ConfigRequestQuestionApp/ConfigRequestQuestionApp/Custom_Scripts/Scripts/Custom/Pages/Builds/ManageBuild.js
@@ -9,6 +9,32 @@
    function InitializeForm() {
 
        var formLoad = new Array;
+       var $versionDrpDwn = $('.versions-drp-dwn .selectpicker');
+       var $questionSlideOut = $('#question-slide-out');
+       var $questionTypeDrpDwn = $('.question-type-drp-dwn .selectpicker');
+       var $buildTree = $('#build-js-tree');
+       var $txtSearchTree = $('#txt-tree-search');
+
+       /*Selected Info*/
+       var selectedQuestion;
+       var availableChildren = new Array();
+
+       /*Question Objects*/
+       var $questionChildrenDrpDwn = $('.q-option-child-input.selectpicker');
+       var $questionOptions = $('ul.q-option-list');
+       var $addQuestionOption = $('i.q-option-add');
+
+       /*BUILD Info*/
+       $buildTitle = $('.manage-build-title h2');
+       $buildName = $('.manage-build-field #build-name');
+
+
+       var newOptionHTML = ('<li class="q-option - list - item"><div class="q-option-manage"><i class="q-option-manage-icon q-option-delete far fa-2 fa-minus-square"></i></div ><input class="form-control q-option-text-input" type="text" value=""><select class="selectpicker show-tick q-option-child-input" title="Choose a child question..." data-style="btn-outline-primary" data-width="100%" data-live-search="true"></select></li>');
+
+       $addQuestionOption.on("click", () => {
+           $("#new-option-placeholder").before(newOptionHTML);
+       });
+
 
         if (window.location.href.indexOf("/Builds/ManageBuild") > -1) {
 
@@ -20,14 +46,14 @@
                 .then((result) => {
                     var $codevalues = result;
                     for (var i = 0; i < $codevalues.length; i++) {
-                        $('.question-type-drp-dwn .selectpicker').append("<option data-tokens='' value='" + $codevalues[i].codeValue + "' >" + $codevalues[i].description + "</option>");
+                        $questionTypeDrpDwn.append("<option data-tokens='' value='" + $codevalues[i].codeValue + "' >" + $codevalues[i].description + "</option>");
                     }
-                    $('.question-type-drp-dwn .selectpicker').selectpicker('refresh');                 
+                    $questionTypeDrpDwn.selectpicker('refresh');                 
                 })
                 .catch((error) => {
-                    $('.question-type-drp-dwn .selectpicker').append("<option data-tokens='' value='1'>Error Loading...</option>");
-                    $('.question-type-drp-dwn .selectpicker').selectpicker('refresh');
-                    $('.question-type-drp-dwn .selectpicker').selectpicker('val', 1).attr('disabled', 'true');
+                    $questionTypeDrpDwn.append("<option data-tokens='' value='1'>Error Loading...</option>");
+                    $questionTypeDrpDwn.selectpicker('refresh');
+                    $questionTypeDrpDwn.selectpicker('val', 1).attr('disabled', 'true');
                     console.log(error);
                 }
                 );
@@ -48,71 +74,101 @@
                         .then((result) => {
 
                             //version_id to load
-                            var $build = result;
-                            var _versionID = $build.SelectedVersion;
-                            var vIDX = $build.BuildVersionList.findIndex(x => x.VersionId === _versionID);
+                            var $buildData = result;
+                            var _versionID = $buildData.SelectedVersion;
+                            var vIDX = $buildData.BuildVersionList.findIndex(x => x.VersionId === _versionID);
 
 
                             //Bind Current Tree
-                            $('#build-js-tree').jstree({
+                            $buildTree.jstree({
                                 'core': {
-                                    'data': $build.BuildVersionList[vIDX].JSTree
+                                    'data': $buildData.BuildVersionList[vIDX].JSTree
                                     , "check_callback": true
                                 }
                                 , "plugins": ["dnd", "search"]
                             });
 
                             //Build Name
-                            $('.manage-build-title h2').text($build.BuildVersionList[vIDX].BuildName);
-                            $('.manage-build-field #build-name').val($build.BuildVersionList[vIDX].BuildName);
+                            $buildTitle.text($buildData.BuildVersionList[vIDX].BuildName);
+                            $buildName.val($buildData.BuildVersionList[vIDX].BuildName);
 
                             //Solution
-                            $('.manage-build-field #solution-meaning').val($build.BuildVersionList[vIDX].SolutionMeaning);
+                            $('.manage-build-field #solution-meaning').val($buildData.BuildVersionList[vIDX].SolutionMeaning);
 
                             //Selected Version
-                            $('.manage-build-field #version-num').val($build.BuildVersionList[vIDX].VersionNum);
+                            $('.manage-build-field #version-num').val($buildData.BuildVersionList[vIDX].VersionNum);
 
                             //Add Versions
-                            for (var i = 0; i < $build.BuildVersionList.length; i++) {
-                                $('.versions-drp-dwn .selectpicker').append("<option data-tokens='' value='" + $build.BuildVersionList[i].VersionId + "' title='Version " + $build.BuildVersionList[i].VersionNum + "' >" + $build.BuildVersionList[i].VersionNum + "</option>");
+                            for (var i = 0; i < $buildData.BuildVersionList.length; i++) {
+                                $versionDrpDwn.append("<option data-tokens='' value='" + $buildData.BuildVersionList[i].VersionId + "' title='Version " + $buildData.BuildVersionList[i].VersionNum + "' >" + $buildData.BuildVersionList[i].VersionNum + "</option>");
                             }
-                            $('.selectpicker').selectpicker('refresh');
-                            $('.selectpicker').selectpicker('val', $build.BuildVersionList[vIDX].VersionId);
+                            $versionDrpDwn.selectpicker('refresh');
+                            $versionDrpDwn.selectpicker('val', $buildData.BuildVersionList[vIDX].VersionId);
 
                             //Last Updated
-                            var date = $build.BuildVersionList[vIDX].VUpdt;
+                            var date = $buildData.BuildVersionList[vIDX].VUpdt;
                             //var nowDate = new Date(parseInt(date.substr(6)));
                             //var tempDate = "";
                             //tempDate += nowDate.format("ddd mmm dd yyyy HH:MM:ss") + " ";
                             $('.manage-build-field #last_update_date').val(date);
 
                             //Last Updated By
-                            $('.manage-build-field #last_updated_by').val($build.BuildVersionList[vIDX].VUpdtName);
+                            $('.manage-build-field #last_updated_by').val($buildData.BuildVersionList[vIDX].VUpdtName);
 
                             //Bind Tree Search
                             var to = false;
-                            $('#txt-tree-search').keyup(function () {
+                            $txtSearchTree.keyup(function () {
                                 if (to) { clearTimeout(to); }
                                 to = setTimeout(function () {
-                                    var v = $('#txt-tree-search').val();
-                                    $('#build-js-tree').jstree(true).search(v);
+                                    var v = $txtSearchTree.val();
+                                    $buildTree.jstree(true).search(v);
                                 }, 250);
                             });
 
 
-                            $('#build-js-tree').on("select_node.jstree", (e, data) => {
+                            $buildTree.on("select_node.jstree", (e, data) => {
                                 //alert("node_id: " + data.node.id);
-                                var qIDX = $build.BuildVersionList[vIDX].QuestionList.findIndex(x => x.QuestionID === parseInt(data.node.id));
-                                var qTitle = $build.BuildVersionList[vIDX].QuestionList[qIDX].QuestionTitle;
+                                //get node object
+                                selectedQuestion = (data.node);
+                                //get children so if the end user wants to make conditional, they know what questions they can select;
+                                availableChildren.splice(0, availableChildren.length);//resets array, and references to array
+                                selectedQuestion.children.forEach((e, i, arr) => availableChildren.push(arr[i]));
+
+                                //clear child questions drop down
+                                $questionChildrenDrpDwn.children().remove().end();
+
+                                //load child drop down
+                                for (var i = 0; i < availableChildren.length; i++) {
+                                    let qIDX = $buildData.BuildVersionList[vIDX].QuestionList.findIndex(x => x.QuestionID === parseInt(availableChildren[i]));
+                                    $questionChildrenDrpDwn.append("<option data-tokens='' value='" +
+                                        availableChildren[i]
+                                        + "' >" + $buildData.BuildVersionList[vIDX]
+                                            .QuestionList[qIDX]
+                                            .QuestionTitle + "</option>");
+                                }
+                                $questionChildrenDrpDwn.selectpicker('refresh');
+
+                                var qIDX = $buildData.BuildVersionList[vIDX].QuestionList.findIndex(x => x.QuestionID === parseInt(selectedQuestion.id));
+                                var qTitle = $buildData.BuildVersionList[vIDX].QuestionList[qIDX].QuestionTitle;
                                 //alert(qTitle);
                                 qTitle += " Extra text that can be type for days. But you know what, maybe. I have NO IDEA. But I sure hope this works.";
 
 
 
-                                $('#question-slide-out').removeClass('d-none').addClass('show-slide');
+                                 $questionSlideOut.removeClass('d-none').addClass('show-slide');
                                 $('#question-text-editor').removeClass('d-none').addClass('show-slide');
                                 $('#question-slide-out .question-tab-inner').attr('data-original-title', qTitle);
                                 $('#question-slide-out .question-tab-inner').text(qTitle.substring(0, 10) + "...");
+                            });
+
+                            //No double click event, have to manually bind.
+                            //https://github.com/vakata/jstree/issues/515
+                            $buildTree.on('dblclick','.jstree-anchor', function (e) {
+                                var instance = $.jstree.reference(this),
+                                    node = instance.get_node(this);
+
+                                 $questionSlideOut.hasClass("q-show") ? null :  $questionSlideOut.addClass("q-show");
+                                
                             });
 
                             $(document.body).on("click", (e) => {
@@ -120,8 +176,8 @@
                                 let container = $(".question-slide-out");
 
                                 if (!container.is(e.target) && container.has(e.target).length === 0) {
-                                    if ($('#question-slide-out').hasClass('q-show')) {
-                                        $('#question-slide-out').removeClass('q-show');
+                                    if ( $questionSlideOut.hasClass('q-show')) {
+                                         $questionSlideOut.removeClass('q-show');
                                         $('#question-text-editor').removeClass('q-show');
                                     }
                                 }
@@ -137,11 +193,11 @@
 
                             $('#question-slide-out .question-tab').on("click", () => {
 
-                                if ($('#question-slide-out').hasClass('q-show')) {
+                                if ( $questionSlideOut.hasClass('q-show')) {
                                     $('#question-text-editor').removeClass('q-show');
                                 }
 
-                                $('#question-slide-out').toggleClass("q-show");
+                                 $questionSlideOut.toggleClass("q-show");
                             });
 
                             //Troubleshooting
